b9013103c03e5bcbf9d3a6c1f0d32f6c3a2296e6
Pawel Moll <pawel.moll@arm.com>
2012-11-19 16:39:07 +0000

    video: Versatile Express DVI output driver
    Versatile Express' DVI video output can be connected to one the three
    sources - motherboard's CLCD controller or a video signal generated
    by one of the daughterboards.
    
    This driver configures the muxer FPGA so the output displays content
    of one of the framebuffers in the system (0 by default, can be changed
    by user writing to the "fb" sysfs attribute of the muxfpga device).
    
    It will also set up the display formatter mode and keep it up
    to date with mode changes requested by the user (eg. with fbset
    tool).
    
    Signed-off-by: Pawel Moll <pawel.moll@arm.com>

---
+++ Documentation/ABI/testing/sysfs-driver-video-vexpress-dvi.c
@@ -0,0 +1,5 @@
+What:		/sys/bus/platform/drivers/vexpress-dvi/<muxfpga device>/fb
+Date:		November 2012
+Contant:	Pawel Moll <pawel.moll@arm.com>
+Description:	This file stores the number of the framebuffer which is
+		supposed to be routed to the board's DVI output.
