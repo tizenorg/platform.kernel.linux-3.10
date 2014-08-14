b0d38a094d30ef4a5e195640a384b41615e52a02
Rob Herring <rob.herring@calxeda.com>
2014-08-14 14:58:42 +0900

    of: introduce common FDT machine related functions
    Introduce common of_flat_dt_match_machine and
    of_flat_dt_get_machine_name functions to unify architectures' handling
    of machine level model and compatible properties.
    
    Several architectures match the root compatible string with an arch
    specific list of machine descriptors duplicating the same search
    algorithm. Create a common implementation with a simple architecture
    specific hook to iterate over each machine's match table.
    
    Change-Id: I77acb5c560e2b08591c37b57d5d87023aa3fbe91
    Signed-off-by: Rob Herring <rob.herring@calxeda.com>
    Acked-by: Grant Likely <grant.likely@linaro.org>

--- include/linux/of_fdt.h
+++ include/linux/of_fdt.h
@@ -117,10 +117,15 @@ extern void early_init_dt_setup_initrd_arch(unsigned long start,
 extern int early_init_dt_scan_root(unsigned long node, const char *uname,
 				   int depth, void *data);
 
+extern const char *of_flat_dt_get_machine_name(void);
+extern const void *of_flat_dt_match_machine(const void *default_match,
+		const void * (*get_next_compat)(const char * const**));
+
 /* Other Prototypes */
 extern void unflatten_device_tree(void);
 extern void early_init_devtree(void *);
 #else /* CONFIG_OF_FLATTREE */
+static inline const char *of_flat_dt_get_machine_name(void) { return NULL; }
 static inline void unflatten_device_tree(void) {}
 #endif /* CONFIG_OF_FLATTREE */
 
