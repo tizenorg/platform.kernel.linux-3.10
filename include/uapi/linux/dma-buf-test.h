593a1c59f0f7f06d1a5e0947c7a0f0b847f5060d
Chanho Park <chanho61.park@samsung.com>
2014-10-24 22:33:50 +0900

    dma-buf: add dma-buf-test driver
    Change-Id: I79e20be7613221c3b638b6ca40b8bbe9489fdcfb
    Signed-off-by: Chanho Park <chanho61.park@samsung.com>

---
+++ include/uapi/linux/dma-buf-test.h
@@ -0,0 +1,11 @@
+#include <linux/ioctl.h>
+
+struct dmabuf_create {
+	__u32 flags;
+	__u32 size;
+};
+
+#define DMABUF_IOCTL_BASE	'D'
+#define DMABUF_IOCTL_CREATE	_IOWR(DMABUF_IOCTL_BASE, 0, struct dmabuf_create)
+#define DMABUF_IOCTL_DELETE	_IOWR(DMABUF_IOCTL_BASE, 1, int)
+#define DMABUF_IOCTL_EXPORT	_IOWR(DMABUF_IOCTL_BASE, 2, int)
