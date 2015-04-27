519743419decbdb024ca97bebf814ecc24f8629e
Paul Osmialowski <p.osmialowsk@samsung.com>
2015-04-27 14:18:02 +0200

    kdbus: after previous commit: these files are not needed anymore
    Change-Id: I290b5ba230f2c5a3b249c5da20a7b8d947d4aa4b
    Signed-off-by: Paul Osmialowski <p.osmialowsk@samsung.com>

--- compat/include/linux/fs.h
+++
@@ -1,9 +0,0 @@
-#ifndef _COMPAT_LINUX_FS_H
-#define _COMPAT_LINUX_FS_H
-
-#include_next <linux/fs.h>
-
-ssize_t vfs_iter_read(struct file *file, struct iov_iter *iter, loff_t *ppos);
-ssize_t vfs_iter_write(struct file *file, struct iov_iter *iter, loff_t *ppos);
-
-#endif /* _COMPAT_LINUX_FS_H */
--- compat/include/linux/uio.h
+++ compat/include/linux/uio.h
@@ -5,18 +5,5 @@
 
 /* other compatibility headers */
 #include <linux/time64.h>
-#include <linux/fs.h>
-
-enum {
-	ITER_IOVEC = 0,
-	ITER_KVEC = 2,
-	ITER_BVEC = 4,
-};
-
-void iov_iter_init(struct iov_iter *i, int direction, const struct iovec *iov,
-			unsigned long nr_segs, size_t count);
-
-void iov_iter_kvec(struct iov_iter *i, int direction, const struct kvec *kvec,
-			unsigned long nr_segs, size_t count);
 
 #endif /* _COMPAT_LINUX_UIO_H */
